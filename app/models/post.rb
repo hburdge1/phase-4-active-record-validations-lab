695328ae4f838104abc045b542cea6145af0aa09
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-02-23 15:02:51 -0800

    commit tree 23979b23ea9c4031ec4757f7beac7a228bc8d30f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(fiction non-fiction)}
+    validates :title_cannot_be_clickbait
+
+    def title_cannot_be_clickbait
+        if title.include?("Won't Believe" || "Secret" || "Top #{1..100}" || "Guess")
+        errors.add(:title, "title contains clickbait language")
+        end
+    end
 end
